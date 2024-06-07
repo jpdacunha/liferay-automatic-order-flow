6d8dafdfd3a3ece457f29218d83396f575dab72b
mbhamami <m.badr.hamami@gmail.com>
2024-06-07 10:40:18 +0200

    Add function to call flowise API

--- liferay-workspace/modules/automatic-order-flow-upgrade/src/main/java/com/inetum/automatic/order/flow/listner/OrderContactListener.java
+++ liferay-workspace/modules/automatic-order-flow-upgrade/src/main/java/com/inetum/automatic/order/flow/listner/OrderContactListener.java
@@ -12,7 +12,12 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
@@ -28,8 +33,8 @@ import org.osgi.service.component.annotations.Component;
 	)
 public class OrderContactListener extends BaseModelListener<AssetEntry>{
 	private static final Log LOGGER = LogFactoryUtil.getLog(OrderContactListener.class);
-
-	@Override
+    private static final String FLOWISE_ENDPOIT="http://flowise:3000/api/v1/prediction/2db83ed9-9ad0-4868-a21e-d4454d0f8cae";
+    @Override
 	public void onAfterUpdate(AssetEntry originalModel, AssetEntry model) throws ModelListenerException {
 		
 		LOGGER.info("Updating  Object ["+model.getClassName()+"] ...");
@@ -48,16 +53,60 @@ public class OrderContactListener extends BaseModelListener<AssetEntry>{
 				LOGGER.info("objectDefinition.getName() = ["+objectDefinition.getName()+"] ");
 				
 				//If OrderContact Object
-				if(objectDefinition.getName().equals("C_OrderContact")) {
+				if(objectDefinition.getName().equals("C_OrderContact")) {					
 					Map<String, Serializable> valuesMap=objectEntry.getValues();
 					LOGGER.info("Desciption ["+valuesMap.get("description")+"] ...");
+					
+					//Calling flowise
+					callFlowise(valuesMap.get("description").toString());
 				}
 				
 			} catch (PortalException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		super.onAfterUpdate(originalModel, model);
+	}
+    
+	private void callFlowise(String requestQuestion) {
+		try {
+            // Creating a URL object
+            URL url = new URL(FLOWISE_ENDPOIT);
+            
+            // Opening a connection
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            
+            // Setting the request method to POST
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            
+            //Send parameter in
+            connection.setDoOutput(true);
+            DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+            out.writeBytes("{\"question\": \""+requestQuestion+"\"}");
+            out.flush();
+            out.close();
+            
+            // Retrieving the response code
+            int responseCode = connection.getResponseCode();
+
+            // Processing the response
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                LOGGER.info("API Response: " + response.toString());
+            } else {
+            	LOGGER.warn("API Call Failed. Response Code: " + responseCode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 }
